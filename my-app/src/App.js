7ae6d94508cea8ab49a67489e2e93c21cbc17bb1
solita-michalguspiel <michal.guspiel@solita.fi>
2023-03-25 12:51:44 +0200

    added spinners package and updated router

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,8 +2,13 @@ import "bootstrap/dist/css/bootstrap.min.css"; // import Bootstrap CSS
 import "bootstrap/dist/js/bootstrap.bundle.min.js"; // import Bootstrap JS
 import "./App.css";
 import NewsPage from "./page/News";
-import Comment from "./page/Comment"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Comment from "./page/Comment";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
 const App = () => {
   return (
@@ -13,6 +18,7 @@ const App = () => {
           <Routes>
             <Route path="/news" element={<NewsPage />} />
             <Route path="/comment/:id" element={<Comment />} />
+            <Route path="*" element={<Navigate to="/news" />} />
           </Routes>
         </div>
       </div>
